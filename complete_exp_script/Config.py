2956ba8e1e26a1b4e71219483da8fac4f386476d
Enrico <enrico.voltan@mail.polimi.it>
2021-03-05 14:09:16 +0100

    Update Config.py

--- complete_exp_script/Config.py
+++ complete_exp_script/Config.py
@@ -11,14 +11,14 @@ n_exp = 2
 
 seeds = [0,11,13,17,23,30,39,912,1237,7591,12,213]
 
-algorithms = ["HoeffdingAdaptiveTree"]#["OzaBag","ImprovedOOB","ImprovedUOB","OOB","UOB","WEOB1","WEOB2","RebalanceStream"]
+algorithms = ["HoeffdingAdaptiveTree","OzaBag","ImprovedOOB","ImprovedUOB","OOB","UOB","WEOB1","WEOB2","RebalanceStream"]
 
-speeds = ["incremental"]#,"sudden", "periodic"]
+speeds = ["incremental","sudden", "periodic"]
 
 reals = ["Elec","PAKDD", "KDDCup"]
 
-drifts = ["appearing-minority"]#, "disappearing-minority", "jitter", "clusters-movement", "appearing-clusters",
-             #"splitting-clusters", "borderline", "shapeshift", "minority-share"]
+drifts = ["appearing-minority", "disappearing-minority", "jitter", "clusters-movement", "appearing-clusters",
+             "splitting-clusters", "borderline", "shapeshift", "minority-share"]
 
 positives = ["1","2","3","4"]
 
